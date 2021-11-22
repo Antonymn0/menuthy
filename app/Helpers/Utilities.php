955f4a0b8761434dabf8a461d41bc1f1c64ef1db
antonymn0 <mnantony0@gmail.com>
2021-11-22 14:06:24 +0300

    fix bugs front end

--- app/Helpers/Utilities.php
+++ app/Helpers/Utilities.php
@@ -10,8 +10,8 @@ class Utilities{
       $names = isset( $validated['full_name'] ) ? explode(" ",$validated['full_name']) : null;
       $validated['first_name'] = isset($names[0]) ? $names[0] : 'user';
       $validated['middle_name'] = count($names)>2 ? $names[1] : null;
-      $validated['last_name'] = count($names) > 2 ? $names[2] : null;
-      $validated['last_name'] = count($names) < 2 ? $names[0] : null;
+      $validated['last_name'] = count($names) > 2 ? $names[2] : $names[0];
+      $validated['last_name'] = count($names) < 2 ? $names[0] :$names[0];
       return $validated;
     }
 }
