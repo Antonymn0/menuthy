72ab17621802f9bbe6b25f81d5c76f20a2373a5c
antonymn0 <mnantony0@gmail.com>
2021-12-22 21:05:05 +0300

    new subscription page

--- resources/js/Pages/Subscriptions/SubscriptionsPage.vue
+++ resources/js/Pages/Subscriptions/SubscriptionsPage.vue
@@ -1,11 +1,11 @@
 <template>
+<div class="parent">
     <div>
         <Header />
-        <TopNavbar />
-        
+        <TopNavbar />        
     </div>
-    <div class="parent"> 
-        <div class="container">
+     
+        <div class="">
             <div class="header text-center">
                 <h1> How much does Menuthy cost? </h1> 
                 <p class="text-muted">There is a reason why we are the best in the market. </p> 
@@ -13,18 +13,18 @@
 
             <div class="bill-period mx-auto py-5"> 
                 <div class="px-1">
-                    <p class="yearly shadow active-yearly" @click.prevent="toggleYearlyTabs($event, 'monthly')"> <span><i class="bi bi-check"></i> </span> Billed Monthly</p>
+                    <p class="yearly shadow active-yearly" @click.prevent="[toggleYearlyTabs($event, 'monthly'),  updatePackagePrice(60, 120, 240)]"> <span><i class="bi bi-check"></i> </span> Billed Monthly</p>
                 </div>
                 <div class="px-1">
-                    <p class="yearly shadow "  @click.prevent="toggleYearlyTabs($event, 'yearly')"> <span ><i class="bi bi-check"></i> </span>  Billed Yearly</p>
+                    <p class="yearly shadow "  @click.prevent="[toggleYearlyTabs($event, 'yearly'),  updatePackagePrice(600, 1200, 2400)]"> <span ><i class="bi bi-check"></i> </span>  Billed Yearly <small class="sm small s font-weight-light">Save 20%</small></p>
                 </div>
             </div>
 <!-- ------------------------ Monthly package panels------------------------------------------ -->
         <div class="monthly-panels ">
             <div class="types py-1 mx-auto"> 
-                <div class="px-2"> <span class="types-tabs shadow active-types" @click.prevent="toggleTypestabs($event)"> Dine-In QR Menu</span> </div>
-                <div class="px-2"> <span class="types-tabs shadow" @click.prevent="toggleTypestabs($event)">Delivery and Pick-Up Menu </span> </div>
-                <div class="px-2"> <span class="types-tabs shadow" @click.prevent="toggleTypestabs($event)">Tablet Menu </span> </div>
+                <div class="px-2"> <span class="types-tabs shadow active-types" @click.prevent="[toggleTypestabs($event), updatePackagePrice(60, 120, 240)]"> Dine-In QR Menu</span> </div>
+                <div class="px-2"> <span class="types-tabs shadow" @click.prevent="[toggleTypestabs($event), updatePackagePrice(120,240,480)]">Delivery and Pick-Up Menu </span> </div>
+                <div class="px-2"> <span class="types-tabs shadow" @click.prevent="[toggleTypestabs($event), updatePackagePrice(60, 120, 240)]">Tablet Menu </span> </div>
             </div>
            
             <div class="packages py-3 mt-4">
@@ -35,11 +35,11 @@
                             <h4 class="pb-4 mb-3 pt-1 text-dark"> Basic</h4>
                             <p class="ty-1 price mb-0"> 
                                 <span class="currency">QAR</span>
-                                <span class="amount">60</span>
+                                <span class="amount">{{this.basic}}</span>
                                 <span class="period">/Month</span>
                             </p>
                             <p class="pb-1 mb-0 text-dark"> <i class="bi bi-check"></i> </p>
-                            <p class=""> <button class="btn btn-danger px-5 d-flex align-items-center mx-auto">  <h5>Purchase </h5>  </button> </p>
+                            <p class=""> <button @click.prevent="purchasePackage(this.basic)" class="btn btn-danger px-5 d-flex align-items-center mx-auto">  <h5>Subscribe </h5>  </button> </p>
                         </div>
                     </div>
                     <div> 
@@ -47,11 +47,11 @@
                             <h4 class="pb-4 mb-3 pt-1 text-dark"> Pro</h4>
                             <p class="ty-1 price mb-0"> 
                                 <span class="currency">QAR</span>
-                                <span class="amount">60</span>
+                                <span class="amount">{{this.pro}}</span>
                                 <span class="period">/Month</span>
                             </p>
                             <p class="pb-1 mb-0 text-dark"> <i class="bi bi-check"></i> </p>
-                            <p class=""> <button class="btn btn-danger px-5 d-flex align-items-center mx-auto">  <h5>Purchase</h5>  </button> </p>
+                            <p class=""> <button  @click.prevent="purchasePackage(this.pro)" class="btn btn-danger px-5 d-flex align-items-center mx-auto">  <h5>Subscribe</h5>  </button> </p>
                         </div>
                     </div>
                     <div> 
@@ -59,11 +59,11 @@
                             <h4 class="pb-4 mb-3 pt-1 text-dark"> Advanced</h4>
                             <p class="ty-1 price mb-0"> 
                                 <span class="currency" >QAR</span>
-                                <span class="amount">60</span>
+                                <span class="amount">{{this.advanced}}</span>
                                 <span class="period">/Month</span>                              
                             </p>
                             <p class="pb-1 mb-0 text-dark"> <i class="bi bi-check"></i> </p>
-                            <p class=""> <button class="btn btn-danger px-5 d-flex align-items-center mx-auto">  <h5>Purchase </h5>  </button> </p>
+                            <p class=""> <button @click.prevent="purchasePackage(this.advanced)" class="btn btn-danger px-5 d-flex align-items-center mx-auto">  <h5>Subscribe </h5>  </button> </p>
                         </div>
                     </div>
                 </div>
@@ -72,9 +72,9 @@
 <!-- ------------------------ Yearly package panels------------------------------------------ -->
         <div class="yearly-panels hidden">
             <div class="types py-1 mx-auto"> 
-                <div class="px-2"> <span class="types-tabs shadow active-types" @click.prevent="toggleTypestabs($event,)"> Dine-In QR Menu</span> </div>
-                <div class="px-2"> <span class="types-tabs shadow" @click.prevent="toggleTypestabs($event)">Delivery and Pick-Up Menu </span> </div>
-                <div class="px-2"> <span class="types-tabs shadow" @click.prevent="toggleTypestabs($event)">Tablet Menu </span> </div>
+                <div class="px-2"> <span class="types-tabs shadow active-types" @click.prevent="[toggleTypestabs($event,),  updatePackagePrice(600, 1200, 2400)]"> Dine-In QR Menu</span> </div>
+                <div class="px-2"> <span class="types-tabs shadow" @click.prevent="[toggleTypestabs($event)], updatePackagePrice(1200, 2400, 4800)">Delivery and Pick-Up Menu </span> </div>
+                <div class="px-2"> <span class="types-tabs shadow" @click.prevent="[toggleTypestabs($event), updatePackagePrice(600, 1200, 2400)]">Tablet Menu </span> </div>
             </div>
             <div class="packages py-3 mt-4">  
                 <p class="py-2 pb-3 text-center mx-auto mb-0" style="font-weight:500; font-size:20px;">Billed Yearly</p>   
@@ -84,11 +84,11 @@
                             <h4 class="pb-4 mb-3 pt-1 text-dark"> Basic </h4>
                             <p class="ty-1 price mb-0"> 
                                 <span class="currency">QAR</span>
-                                <span class="amount">60</span>
+                                <span class="amount">{{this.basic}}</span>
                                 <span class="period">/Yearly</span>
                             </p>
                             <p class="pb-1 mb-0 text-dark"> <i class="bi bi-check"></i> </p>
-                            <p class=""> <button class="btn btn-danger px-5 d-flex align-items-center mx-auto">  <h5>Purchase </h5>  </button> </p>
+                            <p class=""> <button @click.prevent="purchasePackage(this.basic)" class="btn btn-danger px-5 d-flex align-items-center mx-auto">  <h5>Subscribe </h5>  </button> </p>
                         </div>
                     </div>
                     <div> 
@@ -96,11 +96,11 @@
                             <h4 class="pb-4 mb-3 pt-1 text-dark"> Pro</h4>
                             <p class="ty-1 price mb-0"> 
                                 <span class="currency">QAR</span>
-                                <span class="amount">60</span>
+                                <span class="amount">{{this.pro}}</span>
                                 <span class="period">/Yearly</span>
                             </p>
                             <p class="pb-1 mb-0 text-dark"> <i class="bi bi-check"></i> </p>
-                            <p class=""> <button class="btn btn-danger px-5 d-flex align-items-center mx-auto">  <h5>Purchase</h5>  </button> </p>
+                            <p class=""> <button @click.prevent="purchasePackage(this.pro)" class="btn btn-danger px-5 d-flex align-items-center mx-auto">  <h5>Subscribe</h5>  </button> </p>
                         </div>
                     </div>
                     <div> 
@@ -108,11 +108,11 @@
                             <h4 class="pb-4 mb-3 pt-1 text-dark"> Advanced</h4>
                             <p class="ty-1 price mb-0"> 
                                 <span class="currency" >QAR</span>
-                                <span class="amount">60</span>
+                                <span class="amount">{{this.advanced}}</span>
                                 <span class="period">/Yearly</span>                              
                             </p>
                             <p class="pb-1 mb-0 text-dark"> <i class="bi bi-check"></i> </p>
-                            <p class=""> <button class="btn btn-danger px-5 d-flex align-items-center mx-auto">  <h5>Purchase </h5>  </button> </p>
+                            <p class=""> <button @click.prevent="purchasePackage(this.advanced)" class="btn btn-danger px-5 d-flex align-items-center mx-auto">  <h5>Subscribe </h5>  </button> </p>
                         </div>
                     </div>
                 </div>
@@ -120,8 +120,9 @@
          </div>
 
         </div>
+       <Footer /> 
     </div>
-    <Footer />
+    
 </template>
 
 <script>
@@ -136,15 +137,16 @@ export default {
     },
     data(){
         return{
-            basic:0,
-            pro:0,
-            advanced:0,
+            basic:60,
+            pro:120,
+            advanced:240,
         }
     },
     methods:{
         toggleYearlyTabs(event, period){
-            var el = document.getElementsByClassName('yearly');
-             Array.from(el).forEach((item)=>{
+            var el = document.getElementsByClassName('yearly'); 
+            var type = document.getElementsByClassName('types-tabs');           
+            Array.from(el).forEach((item)=>{
                     item.classList.remove('active-yearly');
                 });            
             event.target.classList.add('active-yearly');
@@ -156,6 +158,12 @@ export default {
                 document.getElementsByClassName('monthly-panels')[0].classList.remove('hidden');
                 document.getElementsByClassName('yearly-panels')[0].classList.add('hidden');
             } 
+
+            Array.from(type).forEach((item)=>{
+                    item.classList.remove('active-types');
+                });
+            type[0].classList.add('active-types');
+            type[3].classList.add('active-types');
         },
         updatePackagePrice(basic, pro, advanced){
             this.basic = basic;
@@ -169,9 +177,13 @@ export default {
                 });
             event.target.classList.add('active-types');
         },
+        purchasePackage(subscription){
+            console.log(subscription);
+        }
     },
 }
 </script>
+
 <style scoped>
     .parent{
         font-family:Poppins !important;
@@ -214,7 +226,7 @@ export default {
     .types{
         width:100%; 
         display:flex; 
-            justify-content: center;
+        justify-content: center;
     }
     .types div{
         width:auto;
@@ -242,8 +254,7 @@ export default {
     text-align: center;
 }
 .packages .packages-inner div{
-    flex:1;;
-    
+    flex:1;    
     padding:1rem;
 }
 .packages .packages-inner div div{
@@ -267,6 +278,11 @@ export default {
     font-weight:600;
     font-size:40px;
 }
+.packages .packages-inner div div p{
+   display:flex;
+   align-items: center;
+   justify-content: center;
+}
 .packages .packages-inner div div button{
     border-radius:18px;
 }
@@ -277,14 +293,83 @@ export default {
     color:#fff; 
 }
 .active-types{
-    border-left: 3px solid #ED2130 !important;   
-    border-bottom: 5px solid #ED2130 !important;   
-    
+    border-bottom: 5px solid #ED2130 !important;    
 }
 .hidden{
     display:none;
 }
-.visible{
-    display:block;
+
+/* //media queries */
+  @media only screen and (max-width: 750px) {
+      .parent, footer{
+          padding-top:1rem;
+          background: rgba(243, 242, 242, 0.76);
+      }
+      h1{
+          padding-top:1rem;
+      }
+      .yearly{
+          padding:.4rem !important;
+          padding-right: .3.5rem !important;
+          font-size:20px;
+      }
+      .types px-2{
+        padding-left:5px;
+        padding-right:5px;
+    }
+      .types{ 
+        justify-content: center;
+    }
+    .types-tabs{
+        padding:.3rem !important;
+    }
+     .packages .packages-inner{
+         display:block;
+      }
+    .packages .packages-inner div{
+        width:100%;    
+        padding:1rem;
+    }
+}
+
+  @media only screen and (max-width: 500px) {
+      .parent, footer{
+          background: rgba(243, 242, 242, 0.76);
+      }
+      .yearly{
+          padding:1rem !important;
+          padding-right: .3.5rem !important;
+          font-size:16px;
+      }
+      .types px-2{
+        padding-left:.2rem;
+        padding-right:.2rem;
+        margin:2rem;
+        width:100%;
+    }
+    .bill-period{
+        padding-bottom:.3rem !important;
+    }
+    .types{
+        display:block;
+        padding: 0 !important;
+        text-align: left;
+    }
+    .types div{ 
+        padding:.7rem;
+        margin-bottom:.5rem;
+        width:auto;
+        text-align:center;
+    }
+    .types-tabs{
+        padding:.5rem !important;
+    }
+     .packages .packages-inner{
+         display:block;
+      }
+    .packages .packages-inner div{
+        width:100%;    
+        padding:1rem;
+    }
 }
 </style>
