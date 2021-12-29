80bebd4ff56ecfbe2048fc5dec2021d3496b42c7
antonymn0 <mnantony0@gmail.com>
2021-12-29 12:54:22 +0300

    more stripe tweaks

--- app/Http/Controllers/Api/StripePay/StripePayController.php
+++ app/Http/Controllers/Api/StripePay/StripePayController.php
@@ -104,7 +104,7 @@ class StripePayController extends Controller
     public function updateUser($payment){
         $payment_obj = (object) $payment;         //convert array to object
         $user = User::where('email', $payment_obj->email)->first();
-        if(isset($user)) dd($user->email);
+        if(isset($user)) var_dump($user->email);
         else dd('user not found');
     }
 
