bac7c94cbca1273a13c0ede29143549b9496c42d
antonymn0 <mnantony0@gmail.com>
2021-12-29 08:30:11 +0300

    Stripe integration

--- app/Http/Controllers/Api/StripePay/StripePayController.php
+++ app/Http/Controllers/Api/StripePay/StripePayController.php
@@ -79,7 +79,7 @@ class StripePayController extends Controller
                 // $payment['payment_intent'] = $event->data->payment_intent;
                 // $payment['payment_method'] = $event->data->payment_method;
                 // $payment['reciept_url'] = $event->data->reciept_url;
-                $payment['amount_paid'] = $data;
+                $payment['amount_paid'] = $data->object;
                 return $payment; 
         } 
         if($event->type == 'charge.failed') return $event->type;
