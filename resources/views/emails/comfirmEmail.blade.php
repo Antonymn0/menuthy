34d2674093b8d516632863e2217c901e6785b70f
antonymn0 <mnantony0@gmail.com>
2021-12-29 14:32:58 +0300

    add image to email templates

--- resources/views/emails/comfirmEmail.blade.php
+++ resources/views/emails/comfirmEmail.blade.php
@@ -9,7 +9,7 @@ Please confirm your email address here and get your 15 days trial  instantly.
 Just click the button bellow to confirm your email.
 
 @component('mail::button', ['url' => url('/verify-email/' .$user->email)] )
-Verify email
+Verify
 @endcomponent
 
 Thanks,<br>
