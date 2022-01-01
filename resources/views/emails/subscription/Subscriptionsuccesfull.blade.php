db1b6768810d003eb739dd54f3fd53f1d9b5f850
antonymn0 <mnantony0@gmail.com>
2022-01-01 23:14:54 +0300

    Stripe integration

--- resources/views/emails/subscription/Subscriptionsuccesfull.blade.php
+++ resources/views/emails/subscription/Subscriptionsuccesfull.blade.php
@@ -3,6 +3,7 @@
 Dear {{$subscription->customer_name}}, <br>
 Your subscription to menuthy package has been successful and a payment of {{$subscription->currency}} {{$subscription->amount_paid}} has been recieved . <br>
 Thankyou for choosing our services.
+{{$subscription}}
 
 
 
