e796363f690758a973e2ed9dd093cafdd2348237
antonymn0 <mnantony0@gmail.com>
2022-01-06 09:42:18 +0300

    add columns to subscription_payments_table

--- resources/views/emails/subscription/Subscriptionsuccesfull.blade.php
+++ resources/views/emails/subscription/Subscriptionsuccesfull.blade.php
@@ -1,7 +1,7 @@
 @component('mail::message')
 <img src="http://menuthy.herokuapp.com/images/menuthy_logo_iii.svg" style="width:90px; float:right" alt="menuthy Logo"> <br>
 Dear {{$subscription->customer_name}}, <br>
-Your subscription to menuthy package has been successful and a payment of {{$subscription->currency}} {{$subscription->amount_paid}} has been recieved . <br>
+Your subscription to menuthy {{$subscription->package_type}} {{$subscription->package_period}} package has been successful and a payment of {{$subscription->currency}} {{$subscription->amount_paid}} has been recieved . <br>
 Thankyou for choosing our services.
 
 Kind Regards,<br>
