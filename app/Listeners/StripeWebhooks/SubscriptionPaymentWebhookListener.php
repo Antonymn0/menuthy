89cce2f17c9d40a89f854e5350e832cb3fcd5d0f
antonymn0 <mnantony0@gmail.com>
2022-01-20 13:42:31 +0300

    stripe tweaks

--- app/Listeners/StripeWebhooks/SubscriptionPaymentWebhookListener.php
+++ app/Listeners/StripeWebhooks/SubscriptionPaymentWebhookListener.php
@@ -26,7 +26,7 @@ class SubscriptionPaymentWebhookListener implements shouldQueue
      * @return void
      */
     public function handle(SubscriptionPaymentWebhook $event)    
-    {           die($vent);
+    {           die($event);
       
         $this->handleSubscriptionWebhook( $event);
 
