48d8d12a54e8bfbff733bb056f80db106f4e5a9b
antonymn0 <mnantony0@gmail.com>
2022-01-23 15:28:37 +0300

    stripe pay tweaks

--- app/Listeners/StripeWebhooks/SubscriptionPaymentWebhookListener.php
+++ app/Listeners/StripeWebhooks/SubscriptionPaymentWebhookListener.php
@@ -35,7 +35,7 @@ class SubscriptionPaymentWebhookListener implements shouldQueue
     public function handle(SubscriptionPaymentWebhook $event)    
     {            
         if($event->event->type == 'checkout.session.completed') return true;  
-       
+        if($event->event->data->object->metadata->payment_for !== 'subscription payment') return true;
 
         $this->handleSubscriptionWebhook( $event->event);
     }
@@ -46,7 +46,6 @@ class SubscriptionPaymentWebhookListener implements shouldQueue
      */ 
     public function handleSubscriptionWebhook($event){
         $data = $event -> data;
-        if($data['metadata']['payment_for'] !== 'subscription payment') return true;
         $payment =  array();
             $payment['customer_name'] = $data['object']['billing_details']['name'];
             $payment['email'] = $data['object']['billing_details']['email'];
