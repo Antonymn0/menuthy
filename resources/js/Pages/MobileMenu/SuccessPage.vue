3e1341ab88edc13ad9e494442792fc70d88c6931
antonymn0 <mnantony0@gmail.com>
2022-01-23 17:59:47 +0300

    stripe pay tweaks

--- resources/js/Pages/MobileMenu/SuccessPage.vue
+++ resources/js/Pages/MobileMenu/SuccessPage.vue
@@ -28,9 +28,11 @@ export default {
         this.current_link = this.link;
         setTimeout(() => {
             var local_storage = JSON.parse(localStorage.getItem('menuthy_orders')) || [];
-            local_storage.forEach(order => {
+            if(Object.keys(this.errors).length) {
+                local_storage.forEach(order => {
                 if(order.order_number == this.current_order.order_number) return; // do not save order if already exists
-            });
+                });
+            };           
             local_storage.push(this.current_order);
             localStorage.setItem('menuthy_orders', JSON.stringify(local_storage)); //push order into local storage
         }, 50);
