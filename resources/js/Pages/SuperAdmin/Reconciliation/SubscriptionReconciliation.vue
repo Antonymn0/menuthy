d155f14910543ebc7bde342cfde2dd983074a67f
antonymn0 <mnantony0@gmail.com>
2022-01-24 13:58:56 +0300

    reconciliatio

--- resources/js/Pages/SuperAdmin/Reconciliation/SubscriptionReconciliation.vue
+++ resources/js/Pages/SuperAdmin/Reconciliation/SubscriptionReconciliation.vue
@@ -6,32 +6,44 @@
                 <Sidebar />
             </div>
             <div class="col-md-9 pt-5">
-                <button class="btn btn-primary float-right" @click.prevent="this. getReconciliations()">Refresh</button>
-                <h3> Reconciliation  </h3>
+                <button class="btn btn-success float-right" @click.prevent="this. getReconciliations()">Refresh</button>
+                <h4>Subscription Payments Reconciliation <small class="text-muted">(Incomplete)</small> </h4>
+                <div>
+                    <form class="form-inline">
+                        
+                        <div class="form-group mx-sm-3 mb-2">
+                            <label for="email" class="sr-only">Search by Email</label>
+                            <input type="email" class="form-control" id="iemail" placeholder="Email">
+                        </div>
+                        <button type="submit" class="btn btn-primary mb-2">Search</button>
+                        </form>
+                </div>
                 <div class="table-responsive">
-                    <table class="table-sm tables-striped  p-2">
+                    <table class="table-sm table-striped  p-2">
                         <thead>
                             <th> # </th>
                             <th> Email </th>
                             <th> Name </th>
                             <th> Package type</th>
                             <th>Package  Period </th>
+                            <th>Currency </th>
                             <th> Amount paid </th>
                             <th> Date paid </th>
                             <th> Payment id </th>
                             <th> Reciept </th>
                             <th>Action </th>
                         </thead>
-                        <tr v-for="(payment, index) in this.current_reconciliations" :key="index">
+                        <tr v-for="(payment, index) in this.current_reconciliations" :key="index" class="border-bottom">
                             <td>{{index +1}}</td>
                             <td>{{payment.email}}</td>
                             <td>{{payment.customer_name}}</td>
                             <td>{{payment.package_type}}</td>
                             <td>{{payment.package_period}}</td>
+                            <td>{{payment.currency}}</td>
                             <td>{{payment.amount_paid}}</td>
                             <td>{{payment.created_at}}</td>
                             <td>{{payment.payment_intent}}</td>
-                            <td><span v-if="payment.reciept_url"><a :href="payment.reciept_url" target="black">View reciept</a></span></td>
+                            <td><span v-if="payment.reciept_url"><a :href="payment.reciept_url" class="btn badge-primary" target="black">View reciept</a></span></td>
                             <td>
                                 <span class="btn btn-success">Reconcile</span>
                              </td>
